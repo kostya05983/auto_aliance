1826b9eed08fed9bb68b67a4c249c7ccc70191a4
kubitre <kubitre@gmail.com>
2018-07-12 00:39:24 +0700

    rebase for linear layout

--- app/src/main/java/com/it_zoo/kostya05983/auto_aliance/Franchising/DataSecondSectionUi_Franchising.kt
+++
@@ -1,3 +0,0 @@
-package com.it_zoo.kostya05983.auto_aliance.Franchising
-
-data class DataSecondSectionUi_Franchising(val FirstParagraph: String, val Paragraph2: String, val Paragraph3: String, val Paragraph4: String, val Paragraph5: String?)
